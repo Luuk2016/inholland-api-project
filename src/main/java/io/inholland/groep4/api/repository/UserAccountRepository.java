bca05ced470f3ef61372c39719eec6a85c8b80a2
Luuk Kenselaar <luuk2014@hotmail.com>
2021-06-01 22:04:09 +0200

    Changed primary key datatype

--- src/main/java/io/inholland/groep4/api/repository/UserAccountRepository.java
+++ src/main/java/io/inholland/groep4/api/repository/UserAccountRepository.java
@@ -3,6 +3,6 @@ package io.inholland.groep4.api.repository;
 import io.inholland.groep4.api.model.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
     UserAccount findByIBAN (String IBAN);
 }

567774618c182348352a40ab062c12ff71e4dec7
Luuk Kenselaar <luuk2014@hotmail.com>
2021-06-01 22:38:36 +0200

    GET all and GET specific transactions

--- src/main/java/io/inholland/groep4/api/controller/TransactionsApiController.java
+++ src/main/java/io/inholland/groep4/api/controller/TransactionsApiController.java
@@ -3,6 +3,7 @@ package io.inholland.groep4.api.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.inholland.groep4.api.TransactionsApi;
 import io.inholland.groep4.api.model.Transaction;
+import io.inholland.groep4.api.model.User;
 import io.inholland.groep4.api.service.TransactionService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -12,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +21,7 @@ import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-05-31T22:24:07.069Z[GMT]")
 @RestController
@@ -39,32 +42,21 @@ public class TransactionsApiController implements TransactionsApi {
         this.request = request;
     }
 
+    @PreAuthorize("hasRole('EMPLOYEE')")
     public ResponseEntity<List<Transaction>> getTransactions() {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<Transaction>>(objectMapper.readValue("[ {\n  \"dateTime\" : \"2000-01-23T04:56:07.000+00:00\",\n  \"amount\" : 12345.67,\n  \"sender\" : \"NL91 ABNA 0417 1643 00\",\n  \"reciever\" : \"NL91 ABNA 0417 1643 00\",\n  \"description\" : \"Rolex\",\n  \"id\" : 99999\n}, {\n  \"dateTime\" : \"2000-01-23T04:56:07.000+00:00\",\n  \"amount\" : 12345.67,\n  \"sender\" : \"NL91 ABNA 0417 1643 00\",\n  \"reciever\" : \"NL91 ABNA 0417 1643 00\",\n  \"description\" : \"Rolex\",\n  \"id\" : 99999\n} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<Transaction>>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<List<Transaction>>(HttpStatus.NOT_IMPLEMENTED);
+        // @TODO Get all transactions if the user is an employee, otherwise only the user created transactions
+        List<Transaction> transactions = transactionService.getAllTransactions();
+        return ResponseEntity.status(200).body(transactions);
     }
 
-    public ResponseEntity<Transaction> getSpecificTransaction(@Parameter(in = ParameterIn.PATH, description = "The transaction ID", required=true, schema=@Schema()) @PathVariable("id") Integer id) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Transaction>(objectMapper.readValue("{\n  \"dateTime\" : \"2000-01-23T04:56:07.000+00:00\",\n  \"amount\" : 12345.67,\n  \"sender\" : \"NL91 ABNA 0417 1643 00\",\n  \"reciever\" : \"NL91 ABNA 0417 1643 00\",\n  \"description\" : \"Rolex\",\n  \"id\" : 99999\n}", Transaction.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Transaction>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+    public ResponseEntity<Transaction> getSpecificTransaction(@Parameter(in = ParameterIn.PATH, description = "The transaction ID", required=true, schema=@Schema()) @PathVariable("id") Long id) {
+        // @TODO Only allow getting transactions if owned, or if employee, get all
+        try {
+            Transaction transaction = transactionService.getTransactionById(id);
+            return ResponseEntity.status(200).body(transaction);
+        } catch (IllegalArgumentException iae) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-
-        return new ResponseEntity<Transaction>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     public ResponseEntity<Transaction> postTransactions(@Parameter(in = ParameterIn.DEFAULT, description = "", schema=@Schema()) @Valid @RequestBody Transaction body) {
--- src/main/java/io/inholland/groep4/api/service/TransactionService.java
+++ src/main/java/io/inholland/groep4/api/service/TransactionService.java
@@ -5,8 +5,6 @@ import io.inholland.groep4.api.model.UserAccount;
 import io.inholland.groep4.api.repository.TransactionRepository;
 import io.inholland.groep4.api.repository.UserAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,26 +19,29 @@ public class TransactionService {
     private UserAccountRepository userAccountRepository;
 
     public Transaction add(Transaction transaction) {
-        // @TODO: Check if transaction doesn't already exist :)
-        if (!transactionRepository.existsById(transaction.getId())) {
-            UserAccount sender = userAccountRepository.findByIBAN(transaction.getSender());
-            UserAccount receiver = userAccountRepository.findByIBAN(transaction.getReceiver());
+//        UserAccount sender = userAccountRepository.findByIBAN(transaction.getSender());
+//        UserAccount receiver = userAccountRepository.findByIBAN(transaction.getReceiver());
+//
+//        //if the sender isn't attempting an illegal transaction and doesn't have insufficient funds to complete the transaction
+//        if (!(transaction.getAmount() <= 0.00)) {
+//            if (!((sender.getAccountBalance() - transaction.getAmount()) < sender.getLowerLimit())) {
+//                sender.setAccountBalance(sender.getAccountBalance() - transaction.getAmount());
+//                receiver.setAccountBalance(receiver.getAccountBalance() + transaction.getAmount());
+//
+//                transactionRepository.save(transaction);
+//            }
+//            return transaction;
+//        }
 
-            //if the sender isn't attempting an illegal transaction and doesn't have insufficient funds to complete the transaction
-            if (!(transaction.getAmount() <= 0.00)) {
-                if (!((sender.getAccountBalance() - transaction.getAmount()) < sender.getLowerLimit())) {
-                    sender.setAccountBalance(sender.getAccountBalance() - transaction.getAmount());
-                    receiver.setAccountBalance(receiver.getAccountBalance() + transaction.getAmount());
+        // TEMPORARY CODE BELOW
+        transactionRepository.save(transaction);
 
-                    transactionRepository.save(transaction);
-                }
-                return transaction;
-            }
-        }
         return null;
     }
 
     public List<Transaction> getAllTransactions() {
         return transactionRepository.findAll();
     }
+
+    public Transaction getTransactionById(Long id) { return transactionRepository.getTransactionById(id); }
 }

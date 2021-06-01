a510dddbc37042809d736f5af034feff4547a8d6
Luuk Kenselaar <luuk2014@hotmail.com>
2021-06-01 19:42:17 +0200

    WIP: Add account

--- src/main/java/io/inholland/groep4/api/controller/AccountsApiController.java
+++ src/main/java/io/inholland/groep4/api/controller/AccountsApiController.java
@@ -68,17 +68,11 @@ public class AccountsApiController implements AccountsApi {
     }
 
     public ResponseEntity<UserAccount> postAccount(@Parameter(in = ParameterIn.DEFAULT, description = "", schema=@Schema()) @Valid @RequestBody UserAccount body) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<UserAccount>(objectMapper.readValue("{\n  \"owner\" : \"johndoe\",\n  \"accountStatus\" : \"Active\",\n  \"IBAN\" : \"NL91 ABNA 0417 1643 00\",\n  \"accountType\" : \"Current\",\n  \"lowerLimit\" : 0,\n  \"accountBalance\" : 1271.56\n}", UserAccount.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<UserAccount>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+        try {
+            UserAccount result = userAccountService.add(body);
+            return ResponseEntity.status(200).body(result);
+        } catch (IllegalArgumentException iae) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-
-        return new ResponseEntity<UserAccount>(HttpStatus.NOT_IMPLEMENTED);
     }
-
 }

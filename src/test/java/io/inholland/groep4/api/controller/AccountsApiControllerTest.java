e3900948bb70f3b6d92c57736d19a310c307d361
Luuk Kenselaar <luuk2014@hotmail.com>
2021-06-26 20:19:51 +0200

    Added new unit tests (#28)
    * Added new unit tests
    
    * Finished unit tests

--- src/test/java/io/inholland/groep4/api/controller/AccountsApiControllerTest.java
+++ src/test/java/io/inholland/groep4/api/controller/AccountsApiControllerTest.java
@@ -1,16 +1,16 @@
 package io.inholland.groep4.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.inholland.groep4.api.model.User;
 import io.inholland.groep4.api.model.UserAccount;
-import io.inholland.groep4.api.service.UserAccountService;
-import io.inholland.groep4.api.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,16 +23,49 @@ public class AccountsApiControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    @WithMockUser(username = "john",password = "test", roles = "EMPLOYEE")
+    @WithMockUser(username = "john", password = "test", roles = "EMPLOYEE")
     public void getAccountShouldReturnOk() throws Exception {
-        this.mockMvc.perform(get("/accounts"))
-                .andExpect(status().isOk());
+        this.mockMvc.perform(get("/accounts")).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "john", password = "test", roles = "EMPLOYEE")
+    public void getSpecificAccountShouldReturnOk() throws Exception {
+        this.mockMvc.perform(get("/accounts/1")).andExpect(status().isOk());
     }
 
     @Test
     @WithMockUser(username = "john",password = "test", roles = "EMPLOYEE")
-    public void getSpecificAccountShouldReturnOk() throws Exception {
-        this.mockMvc.perform(get("/accounts/1"))
-                .andExpect(status().isOk());
+    public void createAccountShouldReturnOk() throws Exception
+    {
+        User user = new User();
+        user.setUsername("peter");
+        user.setPassword("test");
+        user.setFirstName("Peter");
+        user.setLastName("Griffin");
+        user.setEmail("peter@example.com");
+        user.setBirthdate("01/01/1970");
+
+        UserAccount userAccount = new UserAccount();
+        userAccount.setAccountType(UserAccount.AccountTypeEnum.CURRENT);
+        userAccount.setOwner(user);
+        userAccount.setAccountBalance(500.00);
+        userAccount.setAccountStatus(UserAccount.AccountStatusEnum.ACTIVE);
+        userAccount.setLowerLimit(100.00);
+
+        this.mockMvc.perform( MockMvcRequestBuilders
+            .post("/accounts")
+            .content(asJsonString(userAccount))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/io/inholland/groep4/api/controller/TransactionsApiControllerTest.java
+++ src/test/java/io/inholland/groep4/api/controller/TransactionsApiControllerTest.java
@@ -1,11 +1,17 @@
 package io.inholland.groep4.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.inholland.groep4.api.model.Transaction;
+import io.inholland.groep4.api.model.User;
+import io.inholland.groep4.api.model.UserAccount;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,13 +27,40 @@ public class TransactionsApiControllerTest {
     @WithMockUser(username = "john",password = "test", roles = "EMPLOYEE")
     public void getTransactionsShouldReturnOk() throws Exception {
         this.mockMvc.perform(get("/transactions"))
-                .andExpect(status().isOk());
+            .andExpect(status().isOk());
     }
 
     @Test
     @WithMockUser(username = "john",password = "test", roles = "EMPLOYEE")
     public void getSpecificTransactionShouldReturnOk() throws Exception {
-        this.mockMvc.perform(get("/transactions/6"))
-                .andExpect(status().isOk());
+        this.mockMvc.perform(get("/transactions/9"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "john",password = "test", roles = "EMPLOYEE")
+    public void createTransactionShouldReturnOk() throws Exception
+    {
+        // Create a new transaction
+        Transaction transaction = new Transaction();
+        transaction.setSender("yeet");
+        transaction.setReceiver("woi");
+        transaction.setAmount(49.95);
+        transaction.setDescription("Omega");
+
+        this.mockMvc.perform( MockMvcRequestBuilders
+            .post("/transactions")
+            .content(asJsonString(transaction))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }

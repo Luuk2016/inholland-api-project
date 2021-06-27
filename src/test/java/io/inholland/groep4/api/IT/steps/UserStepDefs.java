36cebbc2d342f4ae4b5bd3c27dc13861eabe56e3
Davey Smit <davey.smit10@gmail.com>
2021-06-27 14:23:25 +0200

    Update UserStepDefs.java

--- src/test/java/io/inholland/groep4/api/IT/steps/UserStepDefs.java
+++ src/test/java/io/inholland/groep4/api/IT/steps/UserStepDefs.java
@@ -1,8 +1,20 @@
 package io.inholland.groep4.api.IT.steps;
 
 import io.cucumber.java.en.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class UserStepDefs {
+
+    RestTemplate template = new RestTemplate();
+    ResponseEntity<String> responseEntity;
+    String response;
+
+    HttpHeaders headers = new HttpHeaders();
+    // TODO: set urls for all the different endpoint calls that happen in this test
+    //String baseUrl = "http://localhost:8080/authenticate";
+
     @When("a user is authenticated")
     public void aUserIsAuthenticated() {
 

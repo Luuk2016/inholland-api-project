a553a4e572f262edb4e728720971a219ca5720c8
Luuk Kenselaar <luuk@lkenselaar.dev>
2022-05-22 21:47:39 +0200

    Finished the UserService test

--- src/test/java/io/inholland/groep4/api/service/UserServiceTest.java
+++ src/test/java/io/inholland/groep4/api/service/UserServiceTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -43,7 +45,7 @@ public class UserServiceTest {
     }
 
     @Test
-    public void creatingANewUserSuccessfullyShouldGiveObjectBack() {
+    public void creatingANewUserSuccessfullyShouldGiveObject() {
         // Create a new test user
         User createdUser = new User();
         createdUser.setUsername("new-test-user");
@@ -64,7 +66,7 @@ public class UserServiceTest {
     }
 
     @Test
-    public void creatingANewEmployeeSuccessfullyShouldGiveObjectBack() {
+    public void creatingANewEmployeeSuccessfullyShouldGiveUser() {
         // Create a new test employee
         User createdEmployee = new User();
         createdEmployee.setUsername("new-test-employee");
@@ -100,5 +102,92 @@ public class UserServiceTest {
         });
 
         assertTrue(exception.getMessage().contains("Username already in use"));
+    }
+
+    @Test
+    public void updatingAUserSuccessfullyShouldGiveUser() {
+        User user = new User();
+        user.setUsername("test-user2");
+        user.setPassword("password");
+        user.setFirstName("test-user2-firstname");
+        user.setLastName("test-user2-lastname");
+        user.setEmail("testuser2@example.com");
+        // Update the birthdate
+        user.setBirthdate("01/01/1999");
+
+        // Add the employee
+        User response = userService.save(user);
+
+        // Assert that the birthdate has been updated
+        assertEquals(response.getUsername(), user.getUsername());
+        assertEquals(response.getBirthdate(), user.getBirthdate());
+    }
+
+    @Test
+    public void updatingAUserThatDoesNotExistShouldGiveAnException() {
+        // Create a new test user
+        User user = new User();
+        user.setUsername("not-existing-user");
+        user.setPassword("password");
+        user.setFirstName("test-user1-firstname");
+        user.setLastName("test-user1-lastname");
+        user.setEmail("testuser1@example.com");
+        user.setBirthdate("01/01/1970");
+
+        Exception exception = assertThrows(ResponseStatusException.class, () -> {
+            userService.save(user);
+        });
+
+        assertTrue(exception.getMessage().contains("Username not found"));
+    }
+
+    @Test
+    public void gettingAllUsersShouldGiveListOfUsers() {
+        List<User> users = userService.getAllUsers();
+
+        assertThat(users).isNotEmpty();
+        assertThat(users.get(0).getUsername()).isEqualTo("test-employee1");
+    }
+
+    @Test
+    public void findingAUserSuccessfullyByUsernameShouldGiveUser() {
+        String usernameToFind = "test-user1";
+
+        User response = userService.findByUsername(usernameToFind);
+
+        assertNotNull(response);
+        assertThat(response.getUsername()).isEqualTo("test-user1");
+    }
+
+    @Test
+    public void findingAUserWithAnInvalidUsernameShouldGiveException() {
+        String notExistingUsername = "test-user10";
+
+        Exception exception = assertThrows(ResponseStatusException.class, () -> {
+            userService.findByUsername(notExistingUsername);
+        });
+
+        assertTrue(exception.getMessage().contains("Username not found"));
+    }
+
+    @Test
+    public void findingAUserSuccessfullyByIdShouldGiveUser() {
+        Long idToFind = 5L;
+
+        User response = userService.getSpecificUser(idToFind);
+
+        assertNotNull(response);
+        assertThat(response.getUsername()).isEqualTo("test-user2");
+    }
+
+    @Test
+    public void findingAUserWithInvalidIdShouldGiveException() {
+        Long notExistingId = 100L;
+
+        Exception exception = assertThrows(ResponseStatusException.class, () -> {
+            userService.getSpecificUser(notExistingId);
+        });
+
+        assertTrue(exception.getMessage().contains("Id not found"));
     }
 }

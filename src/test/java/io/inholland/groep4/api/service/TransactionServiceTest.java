13da27981c878acbe1359e7d7574c54d710ea045
Davey Smit <davey.smit10@gmail.com>
2022-05-24 16:41:50 +0200

    Create TransactionServiceTest.java

---
+++ src/test/java/io/inholland/groep4/api/service/TransactionServiceTest.java
@@ -0,0 +1,13 @@
+package io.inholland.groep4.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TransactionServiceTest {
+
+    @Autowired
+    private TransactionService transactionService;
+
+
+}

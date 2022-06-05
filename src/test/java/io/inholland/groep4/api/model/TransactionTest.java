3babf95d386633db68453114e9cadc28d72712d5
Davey Smit <54932126+DaveS97@users.noreply.github.com>
2022-06-05 16:23:03 +0200

    Feature/model testing (#41)
    * UserAccountTests
    
    this barely did anything at all for the line coverage, as it turns out, man i love boilerplate that's never actually executed by anything
    
    * forgot to throw away the old test
    
    * Add transaction model tests
    
    * Add user model tests
    
    * Remove unused imports

--- src/test/java/io/inholland/groep4/api/model/TransactionTest.java
+++ src/test/java/io/inholland/groep4/api/model/TransactionTest.java
@@ -1,22 +1,21 @@
 package io.inholland.groep4.api.model;
 
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.threeten.bp.OffsetDateTime;
 
-import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class TransactionTest {
 
     private Transaction createdTransaction;
+    private User createdUser;
 
-    @Test
-    public void createTransactionShouldNotBeNull() {
-        User createdUser = new User();
+    @BeforeEach
+    public void setup() {
+        createdUser = new User();
         createdUser.setUsername("test-employee1");
         createdUser.setPassword("password");
         createdUser.setFirstName("John");
@@ -24,15 +23,59 @@ public class TransactionTest {
         createdUser.setEmail("johndoe@example.com");
         createdUser.setBirthdate("01/01/1970");
 
-        Transaction createdTransaction = new Transaction();
-        createdTransaction.setId(15L);
-        createdTransaction.setOwner(createdUser);
-        createdTransaction.setReceiver("NL01INHO0000000001");
-        createdTransaction.setAmount(100.00);
-        createdTransaction.setDescription("Description");
-        createdTransaction.setRejectionFlag("");
-        createdTransaction.setDateTime(OffsetDateTime.now());
+        createdTransaction = new Transaction();
+    }
 
+    @Test
+    public void createTransactionShouldNotBeNull() {
         assertNotNull(createdTransaction);
+    }
+
+    @Test
+    public void setIdShouldSetThatId() {
+        Long id = 15L;
+        createdTransaction.setId(id);
+        assertEquals(id, createdTransaction.getId());
+    }
+
+    @Test
+    public void setOwnerShouldSetThatOwner() {
+        createdTransaction.setOwner(createdUser);
+        assertEquals(createdUser, createdTransaction.getOwner());
+    }
+
+    @Test
+    public void setReceiverShouldSetThatReceiver() {
+        String IBAN = "NL01INHO0000000001";
+        createdTransaction.setReceiver(IBAN);
+        assertEquals(IBAN, createdTransaction.getReceiver());
+    }
+
+    @Test
+    public void setAmountShouldSetThatAmount() {
+        Double amount = 100.00;
+        createdTransaction.setAmount(amount);
+        assertEquals(amount, createdTransaction.getAmount());
+    }
+
+    @Test
+    public void setDescriptionShouldSetThatDescription() {
+        String description = "Description";
+        createdTransaction.setDescription(description);
+        assertEquals(description, createdTransaction.getDescription());
+    }
+
+    @Test
+    public void setRejectionFlagShouldSetThatRejectionFlag() {
+        String rejectionFlag = "Rejected for reason";
+        createdTransaction.setRejectionFlag(rejectionFlag);
+        assertEquals(rejectionFlag, createdTransaction.getRejectionFlag());
+    }
+
+    @Test
+    public void setDateTimeShouldSetThatDateTime() {
+        OffsetDateTime dateTime = OffsetDateTime.now();
+        createdTransaction.setDateTime(dateTime);
+        assertEquals(dateTime, createdTransaction.getDateTime());
     }
 }
--- src/test/java/io/inholland/groep4/api/model/UserAccountTest.java
+++ src/test/java/io/inholland/groep4/api/model/UserAccountTest.java
@@ -1,24 +1,62 @@
 package io.inholland.groep4.api.model;
 
+import io.inholland.groep4.api.service.UserAccountService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class UserAccountTest {
+    private UserAccount testUserAccount;
+
+    @Autowired
+    UserAccountService service = new UserAccountService();
+
+    @BeforeEach
+    void setup() {
+        testUserAccount = new UserAccount();
+        testUserAccount.setAccountType(UserAccount.AccountTypeEnum.CURRENT);
+        testUserAccount.setAccountBalance(500.00);
+        testUserAccount.setLowerLimit(100.00);
+        testUserAccount.setAccountStatus(UserAccount.AccountStatusEnum.ACTIVE);
+    }
 
     @Test
-    public void createUserAccountShouldNotBeNull() {
-        UserAccount createdUserAccount = new UserAccount();
-        createdUserAccount.setId(15L);
-        createdUserAccount.setIBAN("NL01INHO0000000001");
-        createdUserAccount.setAccountType(UserAccount.AccountTypeEnum.CURRENT);
-        createdUserAccount.setAccountBalance(500.00);
-        createdUserAccount.setLowerLimit(100.00);
-        createdUserAccount.setAccountStatus(UserAccount.AccountStatusEnum.ACTIVE);
+    public void createdUserAccountShouldNotBeNull() {
+        assertNotNull(testUserAccount);
+        System.out.println(testUserAccount);
+    }
 
-        assertNotNull(createdUserAccount);
+    @Test
+    public void setIBANShouldSetThatIBAN() {
+        String IBAN = service.getIBAN();
+        testUserAccount.setIBAN(IBAN);
+        assertEquals(IBAN, testUserAccount.getIBAN());
+    }
+
+    @Test
+    public void setAccountBalanceShouldSetThatAmount() {
+        Double balance = new Random().nextDouble();
+        testUserAccount.setAccountBalance(balance);
+        assertEquals(balance, testUserAccount.getAccountBalance());
+    }
+
+    @Test
+    public void setLowerLimitShouldSetThatAmount() {
+        Double limit = new Random().nextDouble();
+        testUserAccount.setLowerLimit(limit);
+        assertEquals(limit, testUserAccount.getLowerLimit());
+    }
+
+    @Test
+    public void settingNegativeLowerLimitShouldThrowException() {
+        Double limit = new Random().nextDouble();
+        assertThrows(IllegalArgumentException.class, () -> testUserAccount.setLowerLimit(-limit));
     }
 }
 
